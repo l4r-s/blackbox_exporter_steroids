ceda84fd4430e8f1b388def014d5cd5ede47f98a
Lars Bättig <me@l4rs.net>
2024-01-06 12:05:43 +0100

    implements basic auth

--- go.mod
+++ go.mod
@@ -22,11 +22,12 @@ require (
 	github.com/prometheus/client_model v0.5.0 // indirect
 	github.com/prometheus/common v0.45.0 // indirect
 	github.com/prometheus/procfs v0.12.0 // indirect
+	golang.org/x/crypto v0.17.0
 	golang.org/x/mod v0.8.0 // indirect
 	golang.org/x/net v0.17.0 // indirect
 	golang.org/x/oauth2 v0.12.0 // indirect
 	golang.org/x/sys v0.15.0 // indirect
-	golang.org/x/text v0.13.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 	golang.org/x/tools v0.6.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto v0.0.0-20230306155012-7f2fa6fef1f4 // indirect
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"os"
 	"strings"
 	"time"
 
@@ -15,6 +16,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 	io_prometheus_client "github.com/prometheus/client_model/go"
+	"golang.org/x/crypto/bcrypt"
 	"gopkg.in/yaml.v2"
 )
 
@@ -222,8 +224,30 @@ func executeProbe(target string, module config.Module, debug bool) (ProbeResults
 	}, nil
 }
 
+// Auth
+func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Get the Authorization header from the request
+		authHeader := r.Header.Get("Authorization")
+		expectedHash := os.Getenv("BLACKBOX_EXPORTER_AUTH")
+
+		if expectedHash != "disabled" {
+			// Compare the bcrypt hash of the provided token with the stored hash
+			err := bcrypt.CompareHashAndPassword([]byte(expectedHash), []byte(authHeader))
+			if err != nil {
+				// If they don't match, send an unauthorized response and return
+				http.Error(w, "Unauthorized", http.StatusUnauthorized)
+				return
+			}
+		}
+
+		// If authorized, call the next handler
+		next(w, r)
+	}
+}
+
 // Handle http requests
 func main() {
-	http.HandleFunc("/probe", handleProbe)
+	http.HandleFunc("/probe", authMiddleware(handleProbe))
 	http.ListenAndServe(":8080", nil)
 }
